f2775c36caebf5f1e7cdc30f08ccfbbfebc67130
Hubert Kowalski <hubik112@gmail.com>
2026-01-05 19:35:59 +0100

    feat: direct prompting baseline models

--- experiments/baselines/__init__.py
+++ experiments/baselines/__init__.py
@@ -1,4 +1,9 @@
 from .bielik_guard import create_bielik_guard
+from .direct_prompting import DirectPromptingPredictor
+from .direct_prompting_factory import (
+    create_all_prompt_predictors,
+    create_direct_prompting_predictor,
+)
 from .guard_adapters import BielikGuardAdapter, GuardAdapter, LlamaGuardAdapter
 from .guard_model import GuardModel
 from .llama_guard import create_llama_guard
@@ -10,4 +15,7 @@ __all__ = [
     "GuardModel",
     "create_bielik_guard",
     "create_llama_guard",
+    "DirectPromptingPredictor",
+    "create_direct_prompting_predictor",
+    "create_all_prompt_predictors",
 ]
